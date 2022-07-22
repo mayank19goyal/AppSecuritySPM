23b7c33711eb25732d8be0b02d76c9f2d399fdad
Mayank Goyal <mayank.goyal.hcl@kantar.com>
2022-07-22 17:31:20 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AppSecuritySPM
+
+A description of this package.
---
+++ Sources/AppSecuritySPM/AppSecuritySPM.swift
@@ -0,0 +1,6 @@
+public struct AppSecuritySPM {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
